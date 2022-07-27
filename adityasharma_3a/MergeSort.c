25add8ebca3811151b1ec232fa349223f2808042
POP <pop@pop-os.localdomain>
2022-07-28 01:29:40 +0530

    Merge Sort Completed

--- adityasharma_3a/MergeSort.c
+++ adityasharma_3a/MergeSort.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
+#include <memory.h>
+#include "RandomArray.h"
 
 void MergeSorted(int array1[], int l1, int array2[], int l2)
 {
-    //
+    int k = l1-- + l2--;
+    int len = k;
+    int array[k--];
+
+    while(l1 >= 0 && l2 >= 0)
+    {
+        if(array1[l1] > array2[l2])
+            array[k--] = array1[l1--];
+        else
+            array[k--] = array2[l2--];
+    }
+    while(l1 >= 0)
+    {
+        array[k--] = array1[l1--];
+    }
+    while(l2 >= 0)
+    {
+        array[k--] = array2[l2--];
+    }
+
+    memcpy(array1, array, len*4);    
 }
 
 void MergeSort(int array[], int l)
 {
-    if(l > 2)
+    if(l > 1)
     {
         MergeSort(array, l/2);
         MergeSort(array+l/2, l-l/2);
-    }
-    else if(l == 2)
-    {
-        if(array[0] > array[1])
-        {
-            int temp = array[0];
-            array[0] = array[1];
-            array[1] = temp;
-        }
-    }
-    else if(l < 1)
-    {
-        return;
-    }
-    
+        MergeSorted(array, l/2, array+l/2, l-l/2);
+    }    
 }
 
 int main(void)
 {
-    printf("Enter length of array\n");
-    int n;
-    scanf("%d", &n);
-    printf("Enter elements of array\n");
+    int n = 30;
     int array[n];
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%d", &array[i]);
-    }
+    RandomArrayGenerator(array, n);
 
+    clock_t start, end;
+    double cpu_time_used;
+    
+    start = clock();
     MergeSort(array, n);
+    end = clock();
 
-    for(int i = 0; i < n; i++)
-    {
-        printf("%d ", array[i]);
-    }
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Time to execute : %lf \n", time_taken);
 }
