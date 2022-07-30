37ab729f37acff33140a7d267f4019fa3ec7b1e0
Aditya Sharma <aditya.0302.sharma@gmail.com>
2022-07-30 15:04:29 +0530

    Finished Quick Sort and Modifies .gitignore

--- adityasharma_3a/QuickSort.c
+++ adityasharma_3a/QuickSort.c
@@ -1,22 +1,67 @@
 #include <stdio.h>
 #include "RandomArray.h"
 
+int Partition(int array[], int lower, int higher)
+{
+    int left = lower;
+    int right = higher;
+
+    int pivot = array[higher];
+
+    while(left < right)
+    {
+        while (array[left] <= pivot && left < right)
+        {
+            left++;
+        }
+        while (array[right] >= pivot && left < right)
+        {
+            right--;
+        }
+        
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    int temp = array[left];
+    array[left] = array[higher];
+    array[higher] = temp;
+
+    return left;
+}
+
+void QuickSort(int array[], int lower, int higher)
+{
+    if(lower >= higher)
+        return;
+
+    int partitionIndex = Partition(array, lower, higher);
+    QuickSort(array, lower, partitionIndex-1);
+    QuickSort(array, partitionIndex + 1, higher);
+    
+}
+
 int main(void)
 {
     int n = 120;
     int array[n];
     RandomArrayGenerator(array, n);
-
+    
     clock_t start, end;
     double cpu_time_used;
     
     start = clock();
 
-    //Code Here
+    QuickSort(array, 0, n-1);
     
     end = clock();
 
     double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
 
     printf("Time to execute : %lf \n", time_taken);
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
 }
